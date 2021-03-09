08f75dffa4766be2a6fb0bbd9fcf21db2ba9c5aa
Kevin Yung <kevin.yung@etu.univ-grenoble-alpes.fr>
2021-03-09 15:44:45 +0100

    Implémentation fonctionnel du changement de couleur dans le html si la gateway a changé de state

--- reports/change_html.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-replacement="<li><a href='https://lns.campusiot.imag.fr/#/organizations/6/gateways/7276ff0039030716'>7276ff0039030716</a>: KER_FEMTO_030716_P307 - (org 6) - 2021-03-07T19:14:43.804084Z+CHANGED</li>"
-id_to_replace="7276ff0039030716"
-
-sed -i '$id_to_replace/c $replacement' test.html
-#sed -i 's/^/\t\t/' <filename>
